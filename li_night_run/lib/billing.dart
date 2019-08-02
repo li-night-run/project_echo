2d1fdd2ea912e76f686ae4bb819f37ee19051932
Jack Li <jackli@buffalo.edu>
2019-08-02 00:31:26 -0400

    updated basics to main.dart, create dart template (billing, history, profile, and search).

---
+++ li_night_run/lib/billing.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+/*
+ * Building the Billing 
+ * --custom template for each pages
+ * 
+ */
+
+class Billing extends StatelessWidget {
+  // Variables
+
+
+  // Constructor
+  Billing();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+
+    );
+  }
+}
